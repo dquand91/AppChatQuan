590ce22c0cade2a1c95af122c43682767c097285
dquand91 <dquand91@gmail.com>
2018-03-14 17:40:32 +0700

    - Update function Login. - function SignUp

--- app/src/main/java/luongduongquan/com/chatapp/MainActivity.java
+++ app/src/main/java/luongduongquan/com/chatapp/MainActivity.java
@@ -1,13 +1,88 @@
 package luongduongquan.com.chatapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.quickblox.auth.session.QBSettings;
+import com.quickblox.core.QBEntityCallback;
+import com.quickblox.core.exception.QBResponseException;
+import com.quickblox.users.QBUsers;
+import com.quickblox.users.model.QBUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+	static final String APP_ID = "69407";
+	static final String AUTH_KEY = "nOzJV5Oy4HekhKV";
+	static final String AUTH_SECRET = "JepfmhW3YuP8OUr";
+	static final String ACCOUNT_KEY = "WBJHu5bAH9QzskoukPwT";
+
+	Button btnLogin, btnSignUp;
+	EditText edtUser, edtPassword;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		initializeFramworkQuickBlox();
+
+		btnLogin = findViewById(R.id.main_btnLogin);
+		btnSignUp = findViewById(R.id.main_btnSignup);
+		edtUser = findViewById(R.id.main_edtLogin);
+		edtPassword = findViewById(R.id.main_edtPassword);
+
+		btnLogin.setOnClickListener(this);
+		btnSignUp.setOnClickListener(this);
+
+
+
+
+	}
+
+	private void initializeFramworkQuickBlox() {
+
+		QBSettings.getInstance().init(getApplicationContext(), APP_ID, AUTH_KEY, AUTH_SECRET);
+		QBSettings.getInstance().setAccountKey(ACCOUNT_KEY);
+
+	}
+
+
+	@Override
+	public void onClick(View view) {
+
+		switch (view.getId()){
+			case R.id.main_btnLogin:
+
+				String user = edtUser.getText().toString();
+				String password = edtPassword.getText().toString();
+
+				QBUser qbUser = new QBUser(user, password);
+				QBUsers.signIn(qbUser).performAsync(new QBEntityCallback<QBUser>() {
+					@Override
+					public void onSuccess(QBUser qbUser, Bundle bundle) {
+						Toast.makeText(MainActivity.this, "Login success! User = " + qbUser.getLogin().toString() + "", Toast.LENGTH_SHORT ).show();
+					}
+
+					@Override
+					public void onError(QBResponseException e) {
+
+						Toast.makeText(MainActivity.this, "Fail!!!!!!!!!!", Toast.LENGTH_SHORT ).show();
+
+					}
+				});
+
+				break;
+
+			case R.id.main_btnSignup:
+				startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+				break;
+		}
+
 	}
 }
--- app/src/main/java/luongduongquan/com/chatapp/SignUpActivity.java
+++ app/src/main/java/luongduongquan/com/chatapp/SignUpActivity.java
@@ -1,13 +1,82 @@
 package luongduongquan.com.chatapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.quickblox.auth.QBAuth;
+import com.quickblox.auth.session.QBSession;
+import com.quickblox.core.QBEntityCallback;
+import com.quickblox.core.exception.QBResponseException;
+import com.quickblox.users.QBUsers;
+import com.quickblox.users.model.QBUser;
 
 public class SignUpActivity extends AppCompatActivity {
+
+	Button btnSignUp, btnCancel;
+	EditText edtUser, edtPassword;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_up);
+		
+		registerFramworkSession();
+
+		btnSignUp = findViewById(R.id.signup_btnSignup);
+		btnCancel = findViewById(R.id.signup_btnCancel);
+
+		edtUser = findViewById(R.id.signup_edtUser);
+		edtPassword = findViewById(R.id.signup_edtPass);
+
+		btnSignUp.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				String user = edtUser.getText().toString();
+				String password = edtPassword.getText().toString();
+
+				QBUser qbUser = new QBUser(user, password);
+				QBUsers.signUp(qbUser).performAsync(new QBEntityCallback<QBUser>() {
+					@Override
+					public void onSuccess(QBUser qbUser, Bundle bundle) {
+						Toast.makeText(SignUpActivity.this, "SignUp success! User = " + qbUser.getLogin().toString() + "", Toast.LENGTH_SHORT ).show();
+					}
+
+					@Override
+					public void onError(QBResponseException e) {
+						Toast.makeText(SignUpActivity.this, "Sign up fail!!!!!!!!!!", Toast.LENGTH_SHORT ).show();
+					}
+				});
+			}
+		});
+
+		btnCancel.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				finish();
+			}
+		});
+
+
+	}
+
+	private void registerFramworkSession() {
+
+		QBAuth.createSession().performAsync(new QBEntityCallback<QBSession>() {
+			@Override
+			public void onSuccess(QBSession qbSession, Bundle bundle) {
+
+			}
+
+			@Override
+			public void onError(QBResponseException e) {
+				Log.e("Error", e.getMessage() + " ");
+			}
+		});
+
 	}
 }
